e1a2f94833aabb786547c7cd4c4ac41980e79ddc
Isai Sanchez <isanchez@thanksgivingpoint.org>
2026-04-28 10:52:24 -0600

    added proper group for diagnostics keymaps and one for the <leader>a group called actions (autoformat and stuff)

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -12,6 +12,8 @@ return {
 			{ "<leader>w", group = "[W]orkspace" },
 			{ "<leader>t", group = "[T]oggle" },
 			{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+			{ "<leader>x", group = "[X] Diagnostics" },
+			{ "<leader>a", group = "[A]ctions" },
 		},
 	},
 }
