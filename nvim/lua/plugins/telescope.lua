a5bd3f3de7b37cc61a7ed3d1887505f6db3ef38f
Isai Sanchez <isanchez@thanksgivingpoint.org>
2026-04-28 11:24:12 -0600

    now using latest stable branch of telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,7 +8,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	event = "VimEnter",
-	branch = "0.1.x",
+	version = "*",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
