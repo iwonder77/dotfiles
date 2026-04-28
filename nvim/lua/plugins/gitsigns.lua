1bef18978b5e622afcc26d1798891861c0d7a77e
Isai Sanchez <isanchez@thanksgivingpoint.org>
2026-04-28 10:57:29 -0600

    removed conditional block

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,9 +3,6 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	cond = function()
-		return vim.fn.isdirectory(".git") == 1
-	end,
 	opts = {
 		signs = {
 			add = { text = "+" },
